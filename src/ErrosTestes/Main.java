f0aef61779508427b279e50f7386d822f50b223f
Mykael Bruno <mykael.bruno@dcx.ufpb.br>
2025-02-12 10:17:46 -0300

    Início dos estdos de Exceptions

---
+++ src/ErrosTestes/Main.java
@@ -0,0 +1,29 @@
+package ErrosTestes;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            //cria um loop infinito para pedir o scanner
+            try {
+                //"tenta" executar esse bloco
+                System.out.println("Informe um divisor: ");
+                int num = Integer.parseInt(sc.nextLine());
+                //recebe uma sting e converte pra inteiro, para evitar bug de enter no buffer;
+                double res = 5 / num;
+                //divide 5 pelo valor informado >ONDE OCORRE O ERRO<
+                System.out.println(res);
+                //caso não seja 0, ele vai dividir e seguir o código
+                break;
+                //ao seguir o código, chega no break, que finaliza o while e a execução geral do código
+            } catch (ArithmeticException e) {
+                //caso o divisor seja 0, ele gera o erro e pula direto para o catch, para ser tratado, ignorando o break, logo, continuando no loop
+                System.err.println("Erro ao executar: " + e.getMessage());
+                //informa o erro e continua a execução do código, continuando no loop
+            }
+        }
+    }
+}
--- src/codes/AprendendoArrays.java
+++ src/codes/AprendendoArrays.java
@@ -2,23 +2,28 @@ package codes;
 
 //Um array reúne várias variáveis dentro dele
 
+import java.util.Arrays;
+
 public class AprendendoArrays {
     public static void main(String[] args) {
-        int[] array = new int[10]; //criou um array (tipo uma lista) com o nome 'array' do tipo int com 10 'espaços'.
+        int[] numeros = new int[10]; //criou um numeros (tipo uma lista) com o nome 'numeros' do tipo int com 10 'espaços'.
 
         int indice = 0;
-        while (indice < array.length) {
+        while (indice < numeros.length) {
             if (indice % 2 == 0) {
-                array[indice] = indice;
+                numeros[indice] = indice;
             } else {
-                array[indice] = indice * 2;
+                numeros[indice] = indice * 2;
             }
             indice++;
         }
 
         //i++  =>  i+=1
-        for (int i = 0; i < array.length; i++)
-            System.out.println(array[i]);
+        for (int i = 0; i < numeros.length; i++)
+            System.out.println(numeros[i]);
 
+        System.out.println(numeros);
+        System.out.println(Arrays.toString(numeros));
+        System.out.println(numeros.toString());
     }
 }
